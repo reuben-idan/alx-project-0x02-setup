86b6f4e4922a3807c9db3c6cb74f0c66e8a16526
reuben-idan <nobeadonis2@gmail.com>
2025-07-26 12:55:53 +0000

    update code snippets

--- alx-project-0x02/pages/_app.tsx
+++ alx-project-0x02/pages/_app.tsx
@@ -1,12 +1,10 @@
 // pages/_app.tsx
 import type { AppProps } from "next/app";
 import "../styles/globals.css"; // Import your global styles
-import Header from "../components/layout/Header"; // Import the Header component
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Header /> {/* Render the Header component */}
       <Component {...pageProps} /> {/* This renders the current page */}
     </>
   );
