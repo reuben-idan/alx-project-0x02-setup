11463e2e03f79029c74f39ca7e056b33183f9edd
reuben-idan <nobeadonis2@gmail.com>
2025-07-26 10:26:04 +0000

    Update code snippet

---
+++ alx-project-0x02/components/layout/Header.tsx
@@ -0,0 +1,53 @@
+// components/layout/Header.tsx
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="bg-blue-600 text-white p-4 shadow-md">
+      <nav className="container mx-auto flex justify-between items-center">
+        <Link
+          href="/"
+          className="text-2xl font-bold hover:text-blue-200 transition-colors duration-200"
+        >
+          ALX Project
+        </Link>
+        <ul className="flex space-x-6">
+          <li>
+            <Link
+              href="/"
+              className="hover:text-blue-200 transition-colors duration-200"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/posts"
+              className="hover:text-blue-200 transition-colors duration-200"
+            >
+              Posts
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/users"
+              className="hover:text-blue-200 transition-colors duration-200"
+            >
+              Users
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/about"
+              className="hover:text-blue-200 transition-colors duration-200"
+            >
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
